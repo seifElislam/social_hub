699f86e2b67931a1da3aaf73d62a692c76dca4a6
seifElislam <seif.eleslam1990@gmail.com>
2019-10-09 15:26:30 +0200

    Add facebook app Creds.

--- social_hub/settings/dev.py
+++ social_hub/settings/dev.py
@@ -3,4 +3,15 @@ Development Settings
 """
 from social_hub.settings.base import *
 
-
+SOCIAL_AUTH_FACEBOOK_KEY = env('APP_ID')
+SOCIAL_AUTH_FACEBOOK_SECRET = env('APP_SECRET')
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_link']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id, name, email, picture.type(large), link'
+}
+SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = [
+    ('name', 'name'),
+    ('email', 'email'),
+    ('picture', 'picture'),
+    ('link', 'profile_url'),
+]
